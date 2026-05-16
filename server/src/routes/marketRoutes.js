5955a0bea77aba887258aea4c86e1786bee2e4e1
pradashades333 <alexdreve03@gmail.com>
2026-05-16 16:16:22 +0200

    started adding market router

--- server/src/routes/marketRoutes.js
+++ server/src/routes/marketRoutes.js
@@ -1,1 +1,14 @@
-// Market routes
+const express = require("express");
+const marketService = require("../services/marketService");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await marketService.getMarketData();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
