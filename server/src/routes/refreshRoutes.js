e24fa3e316750303d57fd115f93cdde6a210d5f5
pradashades333 <alexdreve03@gmail.com>
2026-05-16 16:43:56 +0200

    added refresh routes

--- server/src/routes/refreshRoutes.js
+++ server/src/routes/refreshRoutes.js
@@ -1,1 +1,21 @@
-// Refresh routes
+const express = require("express");
+const marketService = require("../services/marketService");
+
+const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const data = await marketService.refreshMarketData();
+
+    res.json({
+      message: "Market data refreshed",
+      refreshedAt: data.refreshedAt,
+      itemCount: data.items.length,
+      items: data.items
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router
