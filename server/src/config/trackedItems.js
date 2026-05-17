5d66ee67f00004c1ec1ea7084f37e86babdb5507
pradashades333 <alexdreve03@gmail.com>
2026-05-17 12:33:00 +0200

    chore(server): add tracked item overrides

--- server/src/config/trackedItems.js
+++ server/src/config/trackedItems.js
@@ -1,1 +1,20 @@
-module.exports = [];
+module.exports = [
+  {
+    name: "Sunset",
+    category: "godlies",
+    ebayQueries: [
+      "Murder Mystery 2 Sunset Gun",
+      "MM2 Sunset Gun",
+      "Murder Mystery 2 Sunset"
+    ]
+  },
+  {
+    name: "Sunrise",
+    category: "godlies",
+    ebayQueries: [
+      "Murder Mystery 2 Sunrise Gun",
+      "MM2 Sunrise Gun",
+      "Murder Mystery 2 Sunrise"
+    ]
+  }
+];
